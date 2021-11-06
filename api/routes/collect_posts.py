42205b8f87babb100105d3034d185cc1d7b7f158
xvrgill <60839017+xvrgill@users.noreply.github.com>
2021-11-06 16:21:17 -0400

    added posts to db

--- api/routes/collect_posts.py
+++ api/routes/collect_posts.py
@@ -15,20 +15,17 @@ def store_air_table_posts():
     # Get Air Table posts from Air Table api
     _response = requests.get("http://127.0.0.1:5000/api/air_table/posts")
     r = _response.json()
-    # Extract fields and id from response in reformatted dict
-    final_response = list()
+    # Extract fields and id from response. Store them in a new list for further processing.
+    updated_r = list()
     for post in r:
         field_data = post["fields"]
         _id = post["id"]
         field_data["id"] = _id
-        final_response.append(field_data)
+        updated_r.append(field_data)
 
-    # final_response
+    # Add items in new list to database
+    _response2 = requests.post("http://127.0.0.1:5000/db/air_table/posts", json=updated_r).json()
 
-    # Serialize all posts
-    loaded_data = [AirTablePostSchema().load(item) for item in final_response]
-
-    # TODO: Add them to database
-
-    return jsonify(loaded_data)
+    return _response2
+    # return jsonify(loaded_data)
     # return jsonify(final_response)
